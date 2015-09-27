b8d34ffcb02a64ff21a37cf6fca1dfbdc12cb29b
carolstars1 <carol.vick.gonzalez@gmail.com>
2015-09-27 16:17:07 -0400

    Added 1 new photo and changed jpg to png and a new bootstrap row

--- success.php
+++ success.php
@@ -5,32 +5,32 @@
 
 
     <!-- Line above footer -->
-	  <section>
+	  <section class = "container-fluid">
 			<div class= "row">
 			<h2 class = "new">Lehman College NSBE Success Stories!!! </h2>
 			</div>
 			
 			<div class = "row">
-				<div class = "col span-1-of-4 box">
-					<img alt="Picture Lehman NSBE National Society of Black Engineers College  CUNY " id="success-pictures" src = "image/DanaNY.jpg">
+				<div class = "col-sm-3">
+					<img alt="Picture Lehman NSBE National Society of Black Engineers College  CUNY " id="success-pictures" src = "image/DanaNY.png">
 					<h3> Dana Smith </h3>
-					<p>A Lehman College Graduate, Computer Science major, and former President. She went to the NSBE 40th Annual Conference and competed in a 24-hour Hackathon. Her team won first place and obtained several prizes and recognitions. During the Conference, she had six offeres including Boeing, Dell, and Goldman Sachs. Thanks to NSBE She is now an Full-Time Employee at Goldman Sachs. </p>
+					<p class = "suc">A Lehman College Graduate, Computer Science major, and former President. She went to the NSBE 40th Annual Conference and competed in a 24-hour Hackathon. <br>Her team won first place and obtained several prizes and recognitions. During the Conference, she had six offeres including Boeing, Dell, and Goldman Sachs. Thanks to NSBE She is now an Full-Time Employee at Goldman Sachs. </p>
 				</div>
 				
-				<div class = "col span-1-of-4 box">
-					<img alt="Lehman NSBE  CUNY National Society of Black Engineers College Picture " id="success-pictures" src = "image/carsonCali.jpg">
+				<div class = "col-sm-3">
+					<img alt="Lehman NSBE  CUNY National Society of Black Engineers College Picture " id="success-pictures" src = "image/carsonCali.png">
 					<h3> Carson Brown </h3>
-					<p> A Lehman College Student, Computer Science major, former treasurer, and now Vice President. He went to the NSBE Fall 2014 Regional Conference. He won 2nd place at the 30 &#45; Second pitch competion. He interviewed with several companies including Citibank. He also went to the NSBE 41st Annual conference where he interviewed with IBM and HP. Thanks to NSBE he obtained a summer internship with IBM.</p>
+					<p> A Lehman College Student, Computer Science major, former treasurer, and now Vice President. He went to the NSBE Fall 2014 Regional Conference. He won 2nd place at the 30 &#45; Second pitch competion. <br>He interviewed with several companies including Citibank. He also went to the NSBE 41st Annual conference where he interviewed with IBM and HP. Thanks to NSBE he obtained a summer internship with IBM.</p>
 				</div>
 				
-				<div class = "col span-1-of-4 box">
-					<img alt="NSBE Lehman CUNY National Society of Black Engineers College Picture " id="success-pictures" src = "image/mirna.jpg" >
+				<div class = "col-sm-3">
+					<img alt="NSBE Lehman CUNY National Society of Black Engineers College Picture " id="success-pictures" src = "image/mirna.png" >
 					<h3> Mirna Znait </h3>
-					<p>Lehman College Student, Computer Science and Mathematics major, Tutor, Researcher, and a NSBE member. She has done research with several Lehman College professors. She went to the NSBE Fall 2014 Regional Conference, during the conference she interviewed with serveral companies including Google and Goldman Sachs. After several interviews, she obtained a summer 2015 internship with Goldman Sachs. </p>
+					<p>Lehman College Student, Computer Science and Mathematics major, Tutor, Researcher, and a NSBE member. She has done research with several Lehman College professors. <br> She went to the NSBE Fall 2014 Regional Conference, during the conference she interviewed with serveral companies including Google and Goldman Sachs. After several interviews, she obtained a summer 2015 internship with Goldman Sachs. </p>
 				</div>
 				
-				<div class = "col span-1-of-4 box">
-					<img alt="Lehman CUNY National Society of Black Engineers College Picture NSBE" id="success-pictures" src = "image/ZainNY.jpg">
+				<div class = "col-sm-3">
+					<img alt="Lehman CUNY National Society of Black Engineers College Picture NSBE" id="success-pictures" src = "image/ZainNY.png">
 					<h3> Zain Raza </h3>
 					<p> A Lehman College Student, Computer Science major, and Technical Excellence Chair. He went to the NSBE Fall 2014 Regional Conference. He interviewed with serveral companies including Goldman Sachs. After several interviews he obtained a summer 2015 internship with Goldman Sachs. He has accepted a Full-Time Employment with Goldman Sachs. </p>
 					
@@ -38,30 +38,42 @@
 				
 			</div>
 	<!--</section>
-	  
+
 	  <div class = "clearfix"> </div>
 	<section> -->
+          
+</section>
+
+<section class = "container-fluid">
 			<div class = "row">
-				<div class = "col span-1-of-4 box">
-					<img alt="Lehman  CUNY  College Picture National Society of Black Engineers NSBE " id="success-pictures" src = "image/YaniNY.jpg" alt = "aol intern">
+				<div class = "col-sm-3">
+					<img alt="Lehman  CUNY  College Picture National Society of Black Engineers NSBE " id="success-pictures" src = "image/YaniNY.png" alt = "aol intern">
 					<h3> Yanilda Peralta Ramos</h3>
 					<p> A Lehman College Student, Computer Science major, Ex-LSAMP Researcher, Tutor, and NSBE Programs Chair. She attedend the NSBE Fall 2014 Regional Conference where she interviewed with several companies including Google and Verzion. Thanks to NSBE, she obtained a Data Science internship with AOL.                          </p>
 				</div>
 				
 				
-				<div class = "col span-1-of-4 box">
-					<img  alt=" NSBE CUNY College Picture  Lehman  National Society of Black Engineers" id="success-pictures" src = "image/rodnyCali.jpg">
+				<div class = "col-sm-3">
+					<img  alt=" NSBE CUNY College Picture  Lehman  National Society of Black Engineers" id="success-pictures" src = "image/rodnyCali.png">
 					<h3> Rodny Perez </h3>
 					<p> A transfer student from BMCC, Computer Science Major, LSAMP Researcher, and a NSBE Finance Chair. He attended the NSBE Fall Regional Conference and the NSBE 41st annual conference. During the annual conference he interviewed with Goldman Sachs and submited his resume to an Apple Recruiter. Thanks to NSBE he obtained a summer Web Developer internship with Apple.</p>
 				</div>
 				
-				<div class = "col span-1-of-4 box">
-					<img alt="CUNY NSBE Lehman College  Picture National Society of Black Engineers" id="success-pictures" src = "image/step.jpg">
+				<div class = "col-sm-3">
+					<img alt="CUNY NSBE Lehman College  Picture National Society of Black Engineers" id="success-pictures" src = "image/steve.png">
 					<h3> Stephen Johnson </h3>
 					<p> A Lehman College student, Computer Information Systems major, and NSBE member. He Attended the NSBE Fall 2015 Regional Conference where he interviewed with several companies including Goldman Sachs, Deloitte, and Citibank. He also attented the NSBE 41st Annual Conference where he interviewed with several companies including Microsoft. Thanks to NSBE He obtained a summer internship and accepted a Full-Time position with Citibank. </p>
 					
 				</div>
 				
+                
+                <div class = "col-sm-3">
+                <img src="image/Collin.png" alt="collin goldman sachs">                
+                <h3>Collin Morgan</h3>
+                    
+                
+                </div>
+                
 			</div>
 	</section>
   <!-- Footer -->
